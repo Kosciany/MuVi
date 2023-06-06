5e899fbc9d332f34c0c384e4f52f6dded77e16bc
Karol Adamiak <karol.kosciany@gmail.com>
2023-06-06 19:06:11 +0200

    fixes in command line args

--- main.cpp
+++ main.cpp
@@ -15,21 +15,26 @@ int main(int argc, char** argv) {
     Muvi::renderer_config_t config;
     po::options_description desc("Allowed options");
     desc.add_options()
-        ("help,h", "print usage message")
+        ("help", "print usage message")
         ("debug", "Enable additional logs")
-        ("color,c",
+        ("color",
          po::value<int>(&config.colormap)->default_value(DEFAULT_COLORMAP),
          "Color map to use. Range 0-21. See OpenCV 4.7.0-dev documentation "
          "https://docs.opencv.org/4.x/d3/d50/group__imgproc__colormap.html")
-        ("width,w", po::value<int>(&config.width)->default_value(DEFAULT_WIDTH),
+        ("width", po::value<int>(&config.width)->default_value(DEFAULT_WIDTH),
          "Width of the window")
-        ("height,h",
+        ("height",
          po::value<int>(&config.height)->default_value(DEFAULT_HEIGHT),
          "Height of the window");
 
     po::variables_map vm;
     po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
     po::notify(vm);
+
+    if(vm.count("help")){
+        std::cout << desc << std::endl;
+        return 0;
+    }
 
     MuVi::Logger::Init(vm.count("debug"));
 
